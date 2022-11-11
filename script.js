65b6e21f65d10c255a6386e014aa4d9c6e0765b6
D <Dickerson.Morgan@outlook.com>
2022-11-10 19:36:40 -0500

    added a section on script.js for a button. also added two questions with answers to script

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 // Variables for timer
 var timeEL = document.querySelector(".time");
-var secondsLeft = 100;
-// Variables for game
-
-// Variables for trivia
-
-
-
-// function playGame()
+var secondsLeft = 20;
+// Timer
 function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -21,3 +15,41 @@ function setTime() {
     }, 1000);
 }
 setTime();
+
+// variables for quiz section
+var quizContainer = document.getElementById("questions");
+myQuestions = document.getElementById("questions");
+// quiz questions
+var myQuestions = [
+{
+    question: "Which of the following animals is a common hunting companion of the coyote?", answers: {
+        a: 'Cat',
+        b: 'Badger',
+        c: 'Rabbit',
+        d: 'Mouse'
+    },
+    correctAnswer: 'b'
+},
+{
+    question: "Which animal is known for slapping seals and other aquatic mammals out of the ocean?",
+    answers: {
+        a: 'Sharks',
+        b: 'Whales',
+        c: 'Killer Whales',
+        d: 'Dolphins'
+    },
+    correctAnswer: 'c'
+},
+]
+
+
+
+var targetLanding = document.getElementById("landing");
+var btn = document.getElementById("start");
+btn.oneclick = function () {
+    if (targetLanding.style.display !=="none") {
+        targetLanding.style.display = "none";
+    } else {
+        targetLanding.style.display = "block";
+    }
+};
