bfdc3697cefcca164ddea86a54a49e37bf104a1d
D <Dickerson.Morgan@outlook.com>
2022-11-15 13:21:16 -0500

    moved questions and answers to html. added styles to answer buttons

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
 // variables for timer
 var timeEL = document.querySelector(".time");
-var secondsLeft = 20;
+
 // timer
 function setTime() {
+    var secondsLeft = 80;
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeEL.textContent = "Time: " + secondsLeft;
 
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
-            sendMessage();
         }
-
     }, 1000);
-}
+};
 setTime();
 
-// variables for quiz
-
-// quiz questions
-var questions = [
-{
-    question: "Which of the following animals is the hunting companion of the coyote?", 
-    answers: {
-        a: 'Cat', correct: false,
-        b: 'Badger', correct: true,
-        c: 'Rabbit', correct: false,
-        d: 'Mouse', correct: false
-    },
-},
-{
-    question: "Which animal is known for slapping seals and other mammals out of the ocean?",
-    answers: {
-        a: 'Sharks', correct: false,
-        b: 'Whales', correct: false,
-        c: 'Killer Whales', correct: true,
-        d: 'Dolphins', correct: false
-    },
-},
-{
-    question: "Which animal CANNOT move their jaw sideways?",
-    answers: {
-        a: 'Dogs', correct: false,
-        b: 'Hamsters', correct: false,
-        c: 'Birds', correct: false,
-        d: 'Cats', correct: true
-    },
-}
-]
 // button
-function myFunction() {
+function start() {
     var landing = document.getElementById("landing");
     if (landing.style.display === "none") {
-        landing.style.display = "block";
+        landing.style.display = "block"
     } else {
         landing.style.display = "none";
     }
