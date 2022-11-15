008647c1f61dd117efcd77b51d30b7f4a60d5ace
D <Dickerson.Morgan@outlook.com>
2022-11-15 16:57:56 -0500

    readded question and answer variables to js file. started appending items to page

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-// variables for timer
-var timeEL = document.querySelector(".time");
+// variables for game
+var question = ["Which of the following animals is the hunting companion of the coyote?", "Which animal is known for slapping seals and other mammals out of the ocean?", "Which animal CANNOT move their jaw sideways?"];
+var answer1 = ["a. Moose", "a. Sharks", "a. Dogs"];
+var answer2 = ["b. Badger", "b. Whales", "b. Hamsters",];
+var answer3 = ["c. Rabbit", "c. Killer Whales", "c. Birds",];
+var answer4 = ["d. Mouse", "d. Dolphins", "d. Cats",];
+var correctAnswers = ["b. Badger", "c. Killer Whales", "d. Cats"];
+// appends the game variables to the page
+var body = document.body;
+var h1El = document.createElement("h1");
+h1El.textContent = question
+body.appendChild(h1El)
+
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+li1.textContent = answer1;
+li2.textContent = answer2;
+li3.textContent = answer3;
+li4.textContent = answer4;
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+
+// starts the game itself
+function startQuiz() {
+    timer()
+
+}
 
 // timer
-function setTime() {
+function timer() {
     var secondsLeft = 80;
+    var timeEL = document.querySelector(".time");
+
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeEL.textContent = "Time: " + secondsLeft;
@@ -12,10 +46,11 @@ function setTime() {
             clearInterval(timerInterval);
         }
     }, 1000);
-};
-setTime();
+}
 
-// button
+// button that starts game
+var button = document.querySelector("#start")
+button.addEventListener("click", startQuiz)
 function start() {
     var landing = document.getElementById("landing");
     if (landing.style.display === "none") {
@@ -24,3 +59,4 @@ function start() {
         landing.style.display = "none";
     }
 }
+
